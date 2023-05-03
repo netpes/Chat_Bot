b4ce1b9b7f92c2ce615def08052cd1346d14077e
netpes <arbivnetanel@gmail.com>
2023-05-03 17:30:35 +0300

    split backend and frontend

--- backend/schema/chatsSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const chat_schema = mongoose.Schema({
-  user: { type: String, required: true },
-  admin: { type: String, required: false },
-  chat: { type: Array, required: false },
-});
-
-module.exports = mongoose.model("chats", chat_schema);
